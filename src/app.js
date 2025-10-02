34a84f23df5c9c253ad7dcf81be2377ea9da36de
Dawid <corre14@gmail.com>
2025-10-02 23:23:14 +0200

    feat: implement logging system with Winston and Morgan
    - Add Winston logger configuration
    - Configure Morgan for HTTP request logging
    - Set up log levels and file/console outputs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
+// imports
 import express from "express";
 import cookieParser from "cookie-parser";
+import logger from "#config/logger.js";
+import morgan from "morgan";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(
+  morgan("combined", {
+    stream: { write: (message) => logger.info(message.trim()) },
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to Convo API!");
